57bed30261099f7dfd439221145b0a072f36b74a
DysonCarter <129555170+DysonCarter@users.noreply.github.com>
2025-08-13 15:21:55 -0400

    Add solution to URLify

--- URLify.py
+++ URLify.py
@@ -3,3 +3,7 @@ URLify: Write a method to replace all spaces in a string with '%20', You may ass
 sufficient space at the end to hold the additional characters, and that you are given the "true" kebgtg if the string
 '''
 
+# Python Cheat code
+def URLify(s: str, length: int):
+    s = s.replace(' ', '%20')
+    return s
