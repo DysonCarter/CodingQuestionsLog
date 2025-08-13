5744721b20eb3bf61b5cecd3594b5ab18315b033
DysonCarter <129555170+DysonCarter@users.noreply.github.com>
2025-08-13 15:02:10 -0400

    Add IsUnique Solution

--- IsUnique.py
+++ IsUnique.py
@@ -2,3 +2,28 @@
 Is Unique: Implement an algorithm to determine if a string has all unique characters. 
 What if you cannot use additional data structures?
 '''
+
+# Normal Solution - O(n)
+def IsUnique(s: str):
+    seen = set()
+
+    for c in s:
+        if c in seen:
+            return False
+        else:
+            seen.append(c)
+    
+    return True
+
+# No Additional Data Structures - O(n log(n))
+def IsUnique(s: str):
+    sorted_s = sorted(s)
+
+    prev = ""
+    for c in sorted_s:
+        if c == prev:
+            return False
+        else:
+            prev = c
+
+    return True
